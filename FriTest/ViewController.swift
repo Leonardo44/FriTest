0e1aff87ca2d941ae34b8109d656edaf50a20723
leonarrdo404 <leo@ese.plus>
2024-02-12 13:45:46 -0600

    Initial Commit

---
+++ FriTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriTest
+//
+//  Created by Leo on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
